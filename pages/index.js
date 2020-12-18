68c4b46cae2c63c5f386d2fed185c92b3b4059d2
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2020-12-18 18:36:14 -0300

    img-px

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function IndexPage() {
               </ul>
             </nav>
             <main className="flex flex-1 flex-col button-bg justify-around items-center bg-bg">
-                  <div className="pt-6 pt-2 grid md:grid-cols-2 gap-3 items-center">
+                  <div className="p-2 grid md:grid-cols-2 gap-3 items-center">
                     <img className="w-96 rounded-lg shadow-lg"src="https://instagram.fsdu5-1.fna.fbcdn.net/v/t51.2885-15/e35/121235323_1034363926991662_8717067879844545786_n.jpg?_nc_ht=instagram.fsdu5-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=ynto3FwqmcAAX8OXyEG&tp=1&oh=fd4cfbc929222b4a1a73b120f4d4b061&oe=6000AB9A"/>
                     <img className="w-96 rounded-lg shadow-lg"src="https://instagram.fsdu5-1.fna.fbcdn.net/v/t51.2885-15/e35/131011939_1293549251030098_8448164086003615459_n.jpg?_nc_ht=instagram.fsdu5-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=Uh1AWuTSEEgAX_TsMvP&tp=1&oh=2f3712c5de4191e928ed163f4a83b448&oe=6007C57E"/>
                     <img className="w-96 rounded-lg shadow-lg"src="https://instagram.fsdu5-1.fna.fbcdn.net/v/t51.2885-15/e35/130556329_381807462908232_8986100931247910605_n.jpg?_nc_ht=instagram.fsdu5-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=AxPG_j5o-08AX8HYII2&tp=1&oh=270c0761b4246704ddfab82de1997dee&oe=60079D75"/>
