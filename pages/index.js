48d2816b33d75cf068e6e93f538065293e63cdca
vogelcodes <danielcvt@gmail.com>
2021-02-17 18:24:47 -0300

    img-fix

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,10 @@ export default function IndexPage() {
             </nav>
             <main className="flex flex-1 flex-col button-bg justify-around items-center bg-bg">
                   <div className="p-2 grid md:grid-cols-2 gap-3 items-center">
-                    <img className="w-96 rounded-lg shadow-lg"src="https://instagram.fsdu5-1.fna.fbcdn.net/v/t51.2885-15/e35/121235323_1034363926991662_8717067879844545786_n.jpg?_nc_ht=instagram.fsdu5-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=ynto3FwqmcAAX8OXyEG&tp=1&oh=fd4cfbc929222b4a1a73b120f4d4b061&oe=6000AB9A"/>
-                    <img className="w-96 rounded-lg shadow-lg"src="https://instagram.fsdu5-1.fna.fbcdn.net/v/t51.2885-15/e35/131011939_1293549251030098_8448164086003615459_n.jpg?_nc_ht=instagram.fsdu5-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=Uh1AWuTSEEgAX_TsMvP&tp=1&oh=2f3712c5de4191e928ed163f4a83b448&oe=6007C57E"/>
-                    <img className="w-96 rounded-lg shadow-lg"src="https://instagram.fsdu5-1.fna.fbcdn.net/v/t51.2885-15/e35/130556329_381807462908232_8986100931247910605_n.jpg?_nc_ht=instagram.fsdu5-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=AxPG_j5o-08AX8HYII2&tp=1&oh=270c0761b4246704ddfab82de1997dee&oe=60079D75"/>
-                    <img className="w-96 rounded-lg shadow-lg"src="https://instagram.fsdu5-1.fna.fbcdn.net/v/t51.2885-15/e35/130968278_1096001024163994_7316578535084707412_n.jpg?_nc_ht=instagram.fsdu5-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=ypH_XQmzBZIAX_ZTrZH&tp=1&oh=3738516fcd0b399d4c8cd2e852424293&oe=600530D8"/>
+                    <img className="w-96 rounded-lg shadow-lg"src="/1.jpg"/>
+                    <img className="w-96 rounded-lg shadow-lg"src="/2.jpg"/>
+                    <img className="w-96 rounded-lg shadow-lg"src="/3.jpg"/>
+                    <img className="w-96 rounded-lg shadow-lg"src="/4.jpg"/>
                   </div>
                   <ul className="flex flex-col py-4 gap-4 text-xl text-center text-yellow-900">
         <li className="shadow-lg" >Marcenaria personalizada</li>
